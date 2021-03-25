ff8260cf230d61ad78ffef16d51008a92aa04459
Vadim-16 <vadim16@hotmail.ca>
2021-03-25 16:27:11 -0600

    v0.51 branch broke idea - had to fix git

--- Spring-01-Intro/src/main/java/ca/purpose/edu/ExamFormers/ExamFormerImpl.java
+++ Spring-01-Intro/src/main/java/ca/purpose/edu/ExamFormers/ExamFormerImpl.java
@@ -11,7 +11,6 @@ public class ExamFormerImpl implements ExamFormer {
 
     public ExamFormerImpl(Extractor extractor) {
         questionsWithAnswers = extractor.extractQuestionsWithAnswers();
-        System.out.println(questionsWithAnswers);
         prepareQuestions();
     }
 
