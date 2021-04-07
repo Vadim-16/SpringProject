6e4554b4c659aecf0443ec1c2d7f915074836732
Vadim-16 <vadim16@hotmail.ca>
2021-04-06 20:38:02 -0600

    SpringJdbc - v0.11

--- Spring-05-SpringJDBC/src/main/resources/data.sql
+++ Spring-05-SpringJDBC/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO Books (bookId, `bookTitle`, authorId, genreId) VALUES (1, 'Insomnia', 2, 3);
-INSERT INTO Books (bookId, `bookTitle`, authorId, genreId) VALUES (2, 'Insomnia', 3, 2);
-INSERT INTO Books (bookId, `bookTitle`, authorId, genreId) VALUES (3, 'Insomnia', 1, 1);
+INSERT INTO Books (bookId, `bookTitle`, authorId, genreId) VALUES (2, 'WebServer', 3, 2);
+INSERT INTO Books (bookId, `bookTitle`, authorId, genreId) VALUES (3, 'Photoshop', 1, 1);
 INSERT INTO Authors (authorId, `authorName`) VALUES (1, 'Hosie-Bournar');
 INSERT INTO Authors (authorId, `authorName`) VALUES (2, 'Gellar');
 INSERT INTO Authors (authorId, `authorName`) VALUES (3, 'Hart');
